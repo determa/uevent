0d61e4eaf351964b5493a1048e69d754fb662467
dkorotysh <dkorotysh@stud.khpi.ucode-connect.study>
2023-03-15 00:05:32 +0200

    front etot neponatniy

--- frontend/src/pages/AuthPage.jsx
+++ frontend/src/pages/AuthPage.jsx
@@ -1,16 +1,13 @@
 import AuthForm from "../components/AuthForm";
+import SignUpForm from "../components/SignUpForm";
 import Saly from "../images/Saly-1.svg";
 
 const AuthPage = () => {
     return (
-        <div className="bg-auth-back min-h-screen flex justify-center">
-            <div className="flex items-center">
-                <div className="bg-auth-form">
-                    <img src={Saly} alt="img" />
-                </div>
-                <AuthForm />
-            </div>
-        </div>
+        <div className="bg-auth-back min-h-screen flex justify-center items-center flex-col">
+            <SignUpForm />
+            <AuthForm />
+        </div >
     );
 };
 
