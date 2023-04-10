4346c4446b44c94ff6da553f7a0256116c6a480f
dkorotysh <dkorotysh@stud.khpi.ucode-connect.study>
2023-04-10 19:29:48 +0300

    work

--- backend/controllers/payment_controller.js
+++ backend/controllers/payment_controller.js
@@ -3,9 +3,10 @@ const ApiError = require('../error/ApiError');
 class PaymentController {
     async paymentCallback(req, res, next) {
         try {
-            console.log(req);
+            console.log(req.body)
+            console.log(Buffer.from(req.body.data.toString('utf8'), 'base64').toString('ascii'));
         } catch (error) {
-            console.log(e)
+            console.log(error)
             return next(ApiError.badRequest(e.message));
         }
     }
