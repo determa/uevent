5719e23779efbc2085aba6453b5dfcfe5a9d1c59
dkorotysh <dkorotysh@stud.khpi.ucode-connect.study>
2023-04-11 03:06:04 +0300

    work

--- backend/controllers/ticket_controller.js
+++ backend/controllers/ticket_controller.js
@@ -6,7 +6,7 @@ class TicketController {
         try {
             const { accountId, eventId } = req.dae;
             const { transaction_id } = req.answer;
-            const ticket = await Ticket.create({ accountId, eventId, transaction_id });
+            const ticket = await Ticket.create({ accountId: accountId, eventId: eventId, transaction_id: transaction_id });
             return res.json(ticket);
         } catch (error) {
             console.log(error)
