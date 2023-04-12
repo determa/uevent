cb6334b53edd5ba272c14d155f5222fbaa25889f
dkorotysh <dkorotysh@stud.khpi.ucode-connect.study>
2023-04-12 20:59:34 +0300

    work

--- backend/service/pdfGenerate.js
+++ backend/service/pdfGenerate.js
@@ -16,7 +16,7 @@ module.exports = async (accountId, eventId, transaction_id) => {
         time,
         location: event.location, //change to event.location.name
         transaction_id,
-        // qr_code: 'qr.png',
+        qr_code: 'qr.png',
     };
 
     ejs.renderFile(__dirname + '/template.ejs', params, (err, html) => {
@@ -36,6 +36,7 @@ module.exports = async (accountId, eventId, transaction_id) => {
         const fileName = __dirname + '/file.pdf';
 
         const renderHtml = html.replace(/img src=\"\//g, 'img src="file://' + __dirname + "/");
+        console.log(renderHtml);
 
         pdf.create({ html: renderHtml, path: fileName, data: {}, type: "" }, options)
             .then((res) => {
