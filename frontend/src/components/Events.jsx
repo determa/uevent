d004419873b233b12a436603f9b5c64e80832c92
Жека <jeka20001980@gmail.com>
2023-04-12 17:18:50 +0300

    ok

--- frontend/src/components/Events.jsx
+++ frontend/src/components/Events.jsx
@@ -31,18 +31,24 @@ const SortDropDown = ({ sort, setSort }) => {
                         id="sett"
                         className="absolute right-0 z-10 mt-1 cursor-pointer min-w-[12rem] rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5"
                     >
-                        <div
-                            onClick={() => setSort("date")}
+                        <p
+                            onClick={() => {
+                                setSort("date");
+                                setIsOpen(!isOpen);
+                            }}
                             className="p-3 flex flex-col"
                         >
                             Популярным
-                        </div>
-                        <div
-                            onClick={() => setSort("-date")}
+                        </p>
+                        <p
+                            onClick={() => {
+                                setSort("-date");
+                                setIsOpen(!isOpen);
+                            }}
                             className="p-3 flex flex-col border-t border-gray-400"
                         >
                             Ближайшим
-                        </div>
+                        </p>
                     </div>
                 )}
             </div>
@@ -50,12 +56,8 @@ const SortDropDown = ({ sort, setSort }) => {
     );
 };
 
-const TagsDropDown = ({ allTags, tags, setTags }) => {
+const TagsDropDown = ({ allTags, tags }) => {
     const [isOpen, setIsOpen] = useState(false);
-    console.log(allTags, "_-_", tags);
-    const handler = () => {
-        console.log("tyt");
-    };
     return (
         <>
             <div
@@ -88,6 +90,7 @@ const TagsDropDown = ({ allTags, tags, setTags }) => {
                                     <input
                                         id={data.title}
                                         type="checkbox"
+                                        name={data.title}
                                         className="w-4 h-4 bg-gray-100 border-gray-300 rounded"
                                     />
                                     <label
@@ -114,41 +117,46 @@ const Events = () => {
         { id: 3, title: "Джаз" },
     ];
 
-    const [limit] = useState(10);
-    const [page, setPage] = useState(1);
+    // const [limit] = useState(10);
+    // const [page, setPage] = useState(1); // for pagination
     const [sort, setSort] = useState("date");
     const [tags, setTags] = useState([]);
-    const sortHandler = () => {
-        console.log(tags);
-    };
     // const { data: posts, error } = API.useFetchAllPostsQuery({
     //     limit,
     //     page,
     //     sort,
     // });
 
+    const handler = (e) => {
+        e.preventDefault();
+        const form = new FormData(e.target);
+        const tag = Object.keys(Object.fromEntries(form));
+        setTags(tag);
+        console.log(tag, sort);
+    };
+
     return (
         <>
             <div className="flex flex-col gap-6 mx-auto max-w-2xl md:max-w-4xl lg:max-w-7xl">
-                <div className="flex gap-3 select-none items-center">
+                <form
+                    method="POST"
+                    onSubmit={handler}
+                    className="flex gap-3 select-none items-center"
+                >
                     <div className="flex gap-2 items-center">
                         <span>Сортировать по: </span>
                         <SortDropDown sort={sort} setSort={setSort} />
                     </div>
 
-                    <TagsDropDown
-                        allTags={allTags}
-                        tags={tags}
-                        setTags={setTags}
-                    />
+                    <TagsDropDown allTags={allTags} tags={tags} />
 
                     <button
-                        onClick={sortHandler}
+                        // onClick={sortHandler}
                         className="relative flex w-fit justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500"
                     >
                         применить
                     </button>
-                </div>
+                </form>
                 <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8 drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]">
                     {events &&
                         events.map((event, index) => (
