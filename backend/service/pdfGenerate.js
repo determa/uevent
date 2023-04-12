01ccff38d0c8f22d3dd0f1348c6b5ae57d5c526a
Жека <jeka20001980@gmail.com>
2023-04-12 21:54:35 +0300

    ok

--- backend/service/pdfGenerate.js
+++ backend/service/pdfGenerate.js
@@ -1,14 +1,23 @@
-const qrGenerate = require('./qrGenerate');
-const { Event } = require('../models/models');
+const qrGenerate = require("./qrGenerate");
+const { Event } = require("../models/models");
 const pdf = require("pdf-creator-node");
-const ejs = require('ejs');
-const moment = require('moment');
+const ejs = require("ejs");
+const fs = require("fs");
+const moment = require("moment");
 
 module.exports = async (accountId, eventId, transaction_id) => {
-    const event = await Event.findOne({ where: { id: eventId } });
-    let date = moment(event.date).format('dddd, MMMM Do YYYY');
-    let time = `At ${moment(event.date).format('LT')}`;
+    const event = {
+        title: "asd",
+        location: "dnipro",
+        date: "2023-04-08 19:07:41.109+00",
+    };
+    // const event = await Event.findOne({ where: { id: eventId } });
+    let date = moment(event.date).format("dddd, MMMM Do YYYY");
+    let time = `At ${moment(event.date).format("LT")}`;
     qrGenerate();
+
+    const bitmap = fs.readFileSync(__dirname + "/qr.png");
+    const logo = bitmap.toString("base64");
 
     const params = {
         name: event.title,
@@ -16,10 +25,10 @@ module.exports = async (accountId, eventId, transaction_id) => {
         time,
         location: event.location, //change to event.location.name
         transaction_id,
-        qr_code: `file:/${__dirname}/qr.png`,
+        qr_code: logo,
     };
-
-    ejs.renderFile(__dirname + '/template.ejs', params, (err, html) => {
+    console.log(params.qr_code);
+    ejs.renderFile(__dirname + "/template.ejs", params, (err, html) => {
         if (err) {
             console.log(err);
         }
@@ -29,11 +38,11 @@ module.exports = async (accountId, eventId, transaction_id) => {
             orientation: "portrait",
             childProcessOptions: {
                 env: {
-                    OPENSSL_CONF: '/dev/null',
+                    OPENSSL_CONF: "/dev/null",
                 },
-            }
+            },
         };
-        const fileName = __dirname + '/file.pdf';
+        const fileName = __dirname + "/file.pdf";
         console.log(html);
 
         pdf.create({ html: html, path: fileName, data: {}, type: "" }, options)
@@ -43,6 +52,5 @@ module.exports = async (accountId, eventId, transaction_id) => {
             .catch((error) => {
                 console.error(error);
             });
-
-    })
-}
+    });
+};
