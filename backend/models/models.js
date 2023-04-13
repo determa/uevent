fff3ed303fc7e9bbb805b57d08016e05506b8dd8
dkorotysh <dkorotysh@stud.khpi.ucode-connect.study>
2023-04-14 00:55:01 +0300

    'work'

--- backend/models/models.js
+++ backend/models/models.js
@@ -117,7 +117,7 @@ try {
             type: 'USER',
             confirmed: true,
         }
-    ], { updateOnDuplicate: 'email' });
+    ], { updateOnDuplicate: ['email'] });
 
     User.bulkCreate([
         {
