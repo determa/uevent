0288af6cabca5b52089e6812ee1a36ed907c8cac
dkorotysh <dkorotysh@stud.khpi.ucode-connect.study>
2023-04-13 03:21:44 +0300

    'work'

--- backend/middleware/PaymentCallbackMiddleware.js
+++ backend/middleware/PaymentCallbackMiddleware.js
@@ -2,12 +2,13 @@ const ApiError = require('../error/ApiError');
 
 module.exports = function (req, res, next) {
     try {
-        let answer = Buffer.from(req.body.data.toString('utf8'), 'base64').toString('ascii');
+        let answer = Buffer.from(req.body.data.toString('utf8'), 'base64').toString('utf8');
+        console.log(answer);
         req.answer = JSON.parse(answer);
         if (req.answer.status != "success") {
             return next(ApiError.badRequest("Ошибка при оплате!"));
         }
-        let dae = Buffer.from(req.answer.dae, 'base64').toString('ascii');
+        let dae = Buffer.from(req.answer.dae, 'base64').toString('utf8');
         req.dae = JSON.parse(dae);
         return next();
     } catch (error) {
