b4016259fc4dbe6aadfcb658179512e8e1fff6d3
dkorotysh <dkorotysh@stud.khpi.ucode-connect.study>
2023-04-13 20:46:52 +0300

    'work'

--- backend/service/mailService.js
+++ backend/service/mailService.js
@@ -31,7 +31,7 @@ class MailService {
         this.transporter = nodemailer.createTransport({
             host: process.env.SMTP_HOST,
             port: process.env.SMTP_PORT,
-            secure: false,
+            secure: true,
             auth: {
                 user: process.env.SMTP_USER,
                 pass: process.env.SMTP_PASSWORD,
