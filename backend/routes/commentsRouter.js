0b5b2f760d1c3bb72c8293387fea9f6e8911db21
dkorotysh <dkorotysh@stud.khpi.ucode-connect.study>
2023-04-15 17:30:15 +0300

    'work'

--- backend/routes/commentsRouter.js
+++ backend/routes/commentsRouter.js
@@ -3,7 +3,7 @@ const router = new Router();
 const CommentsController = require("../controllers/comment_controller");
 const authMiddleware = require("../middleware/authMiddleware");
 
-router.get("/", CommentsController.get_comments);
+router.get("/", CommentsController.get_comments_by_event);
 router.post("/", authMiddleware, CommentsController.create);
 router.patch("/:id", authMiddleware, CommentsController.update);
 router.delete("/:id", authMiddleware, CommentsController.delete);
