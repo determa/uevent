ad83e563ed489d3b8d2ebf1f7981decf89454607
Жека <jeka20001980@gmail.com>
2023-04-17 00:13:07 +0300

    'work'

--- backend/service/notification.js
+++ backend/service/notification.js
@@ -1,20 +1,19 @@
-const cron = require('node-cron');
+const cron = require("node-cron");
 const moment = require("moment");
 
 module.exports = async function () {
     try {
-        cron.schedule('* * * * * *', () => {
+        cron.schedule("* * * * * *", () => {
             try {
-                // let date_now = moment().fo;
-                // let date_end = moment(date_now).add(1, 'hours');
-                // console.log(date_now, date_end)
-                console.log('running a task every minute');
-
+                let date_now = moment();
+                let date_end = moment(date_now).add(1, 'hours');
+                console.log(date_now, date_end);
+                console.log("running a task every minute");
             } catch (error) {
-                console.log(error)
+                console.log(error);
             }
         });
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
-}
+};
