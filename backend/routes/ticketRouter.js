2ef6aac630a54c6c7ba24a909022cfd4e821d03d
Жека <jeka20001980@gmail.com>
2023-04-17 00:38:09 +0300

    'work'

--- backend/routes/ticketRouter.js
+++ backend/routes/ticketRouter.js
@@ -4,6 +4,7 @@ const TicketController = require('../controllers/ticket_controller');
 const EventController = require("../controllers/event_controller");
 const PaymentCallbackMiddleware = require('../middleware/PaymentCallbackMiddleware');
 const mailService = require('../service/mailService');
+const authMiddleware = require("../middleware/authMiddleware");
 
 router.get("/", authMiddleware, TicketController.get_all);
 router.post('/callback', PaymentCallbackMiddleware, EventController.tickets_count_decrement, TicketController.create, mailService.sendPDF);
