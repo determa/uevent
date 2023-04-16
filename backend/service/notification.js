15760ad6bfb1fb6d444a53a75bf0b4884b54146c
dkorotysh <dkorotysh@stud.khpi.ucode-connect.study>
2023-04-17 00:07:32 +0300

    'work'

--- backend/service/notification.js
+++ backend/service/notification.js
@@ -1,7 +1,20 @@
 const cron = require('node-cron');
+const moment = require("moment");
 
 module.exports = async function () {
-    cron.schedule('* * * * * *', () => {
-        console.log('running a task every minute');
-    });
+    try {
+        cron.schedule('* * * * * *', () => {
+            try {
+                // let date_now = moment().fo;
+                // let date_end = moment(date_now).add(1, 'hours');
+                // console.log(date_now, date_end)
+                console.log('running a task every minute');
+
+            } catch (error) {
+                console.log(error)
+            }
+        });
+    } catch (error) {
+        console.log(error)
+    }
 }
