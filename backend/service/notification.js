c2d7990a0948ab49c373e749cfc1517ebc17c178
dkorotysh <dkorotysh@stud.khpi.ucode-connect.study>
2023-04-17 18:44:21 +0300

    'work'

--- backend/service/notification.js
+++ backend/service/notification.js
@@ -6,8 +6,8 @@ module.exports = async function () {
     try {
         cron.schedule("* * * * * *", () => {
             try {
-                let date_now = moment().hour(0).minute(0);
-                let date_end = moment().hour(23).minute(59);
+                let date_now = moment().hour(0).minute(0).second(0);
+                let date_end = moment().hour(23).minute(59).second(59);
                 console.log(date_now, date_end);
                 console.log("running a task every minute ");
             } catch (error) {
