3b1f91e4f7f35fcfcfe2dc13c5865afeec8254e2
dkorotysh <dkorotysh@stud.khpi.ucode-connect.study>
2023-04-17 21:38:28 +0300

    'work'

--- backend/middleware/ErrorHandler.js
+++ backend/middleware/ErrorHandler.js
@@ -4,5 +4,6 @@ module.exports = (err, req, res, next) => {
     if (err instanceof ApiError) {
         return res.status(err.status).json({ message: err.message });
     }
+    console.log(err);
     return res.status(500).json({ message: "unknown error" });
 };
