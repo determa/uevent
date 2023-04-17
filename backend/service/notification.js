1e63614068b29c2efbc25d9f3e137e88fa261b47
dkorotysh <dkorotysh@stud.khpi.ucode-connect.study>
2023-04-17 19:03:14 +0300

    'work'

--- backend/service/notification.js
+++ backend/service/notification.js
@@ -6,7 +6,7 @@ const { Op } = require("sequelize");
 module.exports = async function () {
     // 0 0 6 */1 * *
     try {
-        cron.schedule("* * * * * *", () => {
+        cron.schedule("* * * * * *", async () => {
             try {
                 let date_now = moment().hour(0).minute(0).second(0);
                 let date_end = moment().hour(23).minute(59).second(59);
