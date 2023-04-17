85a7251875bde7571db8beecd6e6a76eba3d77bc
dkorotysh <dkorotysh@stud.khpi.ucode-connect.study>
2023-04-17 21:33:43 +0300

    'work'

--- backend/index.js
+++ backend/index.js
@@ -57,7 +57,7 @@ const start = async () => {
         const ConnectSession = Connect(session)
         const sessionStore = new ConnectSession({
             conObject: {
-                connectionString: `postgres://adminjs:adminjs@${process.env.HOST}:5432/adminjs`,
+                connectionString: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.HOST}:5432/uevent`,
                 ssl: process.env.NODE_ENV === 'production',
             },
             tableName: 'session',
