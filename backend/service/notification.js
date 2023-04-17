bd5ea0cfef40514c35fa615e4ed710ecb3731520
dkorotysh <dkorotysh@stud.khpi.ucode-connect.study>
2023-04-17 19:49:45 +0300

    'work'

--- backend/service/notification.js
+++ backend/service/notification.js
@@ -7,7 +7,7 @@ const { sendNotificationByEvent } = require("./mailService");
 module.exports = async function () {
     // 0 0 6 */1 * *
     try {
-        cron.schedule("* * * * * *", async () => {
+        cron.schedule("*/10 * * * * *", async () => {
             try {
                 let date_now = moment().hour(0).minute(0).second(0);
                 let date_end = moment().hour(23).minute(59).second(59);
