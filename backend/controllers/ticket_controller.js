96545da64e139962d8ec1d74106cbe6f4b107563
determa <jeka20001980@gmail.com>
2023-04-18 22:39:12 +0000

    work

--- backend/controllers/ticket_controller.js
+++ backend/controllers/ticket_controller.js
@@ -1,18 +1,22 @@
 const ApiError = require('../error/ApiError');
 const { Ticket, Event } = require('../models/models');
 const pdfGenerate = require('../service/pdfGenerate');
+const qrGenerate = require('../service/qrGenerate');
 
 class TicketController {
     async get_all(req, res, next) {
         try {
             const { accountId } = req.account;
             const favorite = await Ticket.findAll({
-                where: { accountId }, 
-                include: [{ 
-                    model: Event, 
+                where: { accountId },
+                include: [{
+                    model: Event,
                     attributes: ['id', 'title', 'date', 'location', 'picture'],
                 }]
             });
+            for (const element of favorite) {
+                element.dataValues.qr_code = await qrGenerate(element.dataValues.event.dataValues.id);
+            }
             res.json(favorite)
         } catch (error) {
             console.log(error);
